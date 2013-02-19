66e3f32bd4567665c7977a2c05f06ccb9ca5ca37
James Lakey <jameslakey@me.com>
2013-02-19 15:03:25 -0500

    adding modules and config plus a node mod

---
+++ modules/lib/config.js
@@ -0,0 +1,33 @@
+var config = (function(){
+    appName: "CME Database App",
+    host:"127.0.0.1",
+    user:"root",
+    password: "ronky",
+    db:"cme"
+}); 
+
+module.exports = config;
+
+// var config = (function( config, $){
+//   var app = {};
+//   app.appName = "CME Test Site";
+//   app.env = {
+//     production: false,
+//     staging: false,
+//     test: false,
+//     development: false
+//   }
+//   app.server = {
+//     port: 3000,
+//     ip: '127.0.0.1' 
+//   }
+//   app.db = {
+//     type: 'mysql',
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'ronky',
+//     db: 'cme'
+//   }
+//   return config;
+
+// })(config, jQuery);
---
+++ modules/src/config2.coffee
@@ -0,0 +1,9 @@
+song = -> 
+	inner = -1
+	outer = 12
+
+config = ->
+	appName: "My Pimp App"
+	host: "127.0.0.1"
+
+module.exports = config;
