bb70490551fe6c6472beb5e1db979d5925eecc36
James Lakey <jameslakey@me.com>
2013-02-19 15:04:31 -0500

    adding modules and config plus a node mod 2

---
+++ public/javascripts/config.js
@@ -0,0 +1,29 @@
+// var settings = {
+
+// 	'port': 3000
+// 	, 'title': 'My App Testing'
+
+// }
+// module.exports = settings;
+
+// exports.config = function(){
+//   config.Aname = "CME Test Site";
+//   config.env = {
+//     production: false,
+//     staging: false,
+//     test: false,
+//     development: false
+//   }
+//   config.server = {
+//     port: 3000,
+//     ip: '127.0.0.1' 
+//   }
+//   config.db = {
+//     type: 'mysql',
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'ronky',
+//     db: 'cme'
+//   }
+//    return config;
+// }
